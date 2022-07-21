f6a2441ebdc15f817b24073938a15721e1c56bed
tash7540 <59746687+tash7540@users.noreply.github.com>
2022-07-21 12:04:02 -0600

    implemented contact me functionality

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,21 +1,43 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
-
+import nodemailer from 'nodemailer';
 const router = express.Router();
 
 export const createPost = async (req, res) => {
     const {name,email,subject,message } = req.body;
-    console.log(name);
-    console.log(req.body);
-    const newPostMessage = new PostMessage({ name,email,subject,message})
-    try {
-        await newPostMessage.save();
 
-        res.status(201).json(newPostMessage );
-    } catch (error) {
-        res.status(409).json({ message: error.message });
-    }
+    const contactEmail = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: "tamer.shahwan.jobs@gmail.com",
+        pass: process.env.PASSWORD,
+      },
+    });
+    contactEmail.verify((error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Ready to Send");
+      }
+    });
+    const mail = {
+      from: name,
+      to: "tamer.shahwan.jobs@gmail.com",
+      subject: subject,
+      html: `<p>Name: ${name}</p>
+          <p>Subject: ${subject}</p>
+          <p>Email: ${email}</p>
+          <p>Message: ${message}</p>`,
+      };
+    contactEmail.sendMail(mail, (error) => {
+        if (error) {
+            res.json({ status: "ERROR" });
+        } else {
+            res.json({ status: "Message Sent" });
+        }
+      });
+
 }
 
 
