a64e9759dfd5a35b9cc86216ca67c0036ab9caec
Tamer Shahwan <59746687+tash7540@users.noreply.github.com>
2022-07-21 12:57:37 -0600

    Delete backend directory

--- backend/routes/posts.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {createPost} from '../controllers/posts.js';
-
-const router = express.Router();
-
-router.post('/sendMessage', createPost);
-
-export default router;
